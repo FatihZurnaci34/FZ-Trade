709c5ac69cca296bdbff8d5a26bcbb19969ec35a
fatihzurnaci34 <fatihzurnaci34@gmail.com>
2023-02-23 18:31:48 +0300

    Add project files.

---
+++ src/projects/fzTrade/FzTrade.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace FzTrade.Infrastructure
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ src/projects/fzTrade/FzTrade.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FzTrade.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
+                                                                IConfiguration configuration)
+        {
+            services.AddDbContext<BaseDbContext>(options =>
+                                                     options.UseSqlServer(
+                                                         configuration.GetConnectionString("FZTradeConnectionString")));
+            //services.AddScoped<IBrandRepository, BrandRepository>();
+            //services.AddScoped<IModelRepository, ModelRepository>();
+            //services.AddScoped<IUserRepository, UserRepository>();
+            //services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+            //services.AddScoped<IOperationClaimRepository, OperationClaimRepository>();
+            //services.AddScoped<IUserOperationClaimRepository, UserOperationClaimRepository>();
+
+            return services;
+        }
+    }
+}
